e20f9dd3f6b28d481dbc82897fd5a72c0cf6f5ec
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-23 11:25:15 -0700

    If the query response includes any doctors, the following information should be included about each doctor: first name, last name, address, phone number and whether or not the doctor is accepting new patients

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ $(document).ready(function() {
     const search = $("#search").val();
     const name = $("#name").val();
     $(".output").empty();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+    // 45.523062, -122.676482
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=45.523062%2C-122.676482%2C100&user_location=45.523062%2C-122.676482&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
     getItem(fetchResults, badResults, search, url)
   });
 });
@@ -19,18 +20,28 @@ $(document).ready(function() {
 let fetchResults = function(response, url){
   let items = JSON.parse(response);
 
-  // console.log(items.data[0].profile.first_name);
-  // console.log(items.data[0].profile.last_name);
-  // console.log(items.data[0].practices[0].name);
+  // Could not find website
   let i = 0;
   items.data.forEach(function(item) {
-    // let x = 0;
-    // $(".doctoroutput").append("Practices: <br>");
-    // items.data[i].practices[x].forEach(function(item) {
-    //   $(".doctoroutput").append(items.data[i].practices[x].name);
-    //   x += 1;
-    // });
-    $(".doctoroutput").append(items.data[i].profile.first_name + " " + items.data[i].profile.last_name + "<br>");
+    $(".doctoroutput").append("<b>Doctor</b>: <br>" + items.data[i].profile.first_name + " " + items.data[i].profile.last_name + "<br>");
+
+    let x = 0;
+    $(".doctoroutput").append("<b>Practice(s)</b>: <br>");
+    items.data[i].practices.forEach(function(item) {
+      $(".doctoroutput").append("Location: " + items.data[i].practices[x].name + "<br>Address: " + items.data[i].practices[x].visit_address.street + ", " + items.data[i].practices[x].visit_address.city + " " + items.data[i].practices[x].visit_address.state + ", " + items.data[i].practices[x].visit_address.zip + "<br>" + "Phone Number(s): <br>");
+
+      let z = 0;
+      items.data[i].practices[x].phones.forEach(function(item) {
+        $(".doctoroutput").append(items.data[i].practices[x].phones[z].number + "<br>")
+        z += 1;
+      });
+
+      $(".doctoroutput").append("Currently accepting patients? " + items.data[i].practices[x].accepts_new_patients + "<br>");
+
+      x += 1;
+      $(".doctoroutput").append("<br>");
+    });
+    $(".doctoroutput").append("<hr><br>");
     i += 1;
   });
 
